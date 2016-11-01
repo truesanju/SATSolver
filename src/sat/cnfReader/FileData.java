1c52cb27125c38a3ddbe3f2fba21cec165c0682c
crinklecut <charlescrinkle@gmail.com>
2016-11-01 22:36:50 +0800

    Add files via upload

---
+++ src/sat/cnfReader/FileData.java
@@ -0,0 +1,25 @@
+package readCNF;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Charles on 1/11/2016.
+ */
+public class FileData {
+    public static void main(String[] args) throws IOException{
+
+        String file_name =  "C:/Users/Charles/Desktop/largeSat.CNF";
+
+        try{
+            ReadFile file = new ReadFile(file_name);
+            System.out.println(file.OpenFile());
+
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+}
---
+++ src/sat/cnfReader/ReadFile.java
@@ -0,0 +1,65 @@
+package readCNF;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/**
+ * Created by Charles on 1/11/2016.
+ */
+public class ReadFile {
+
+    private String path;
+
+    public ReadFile(String file_path) {
+        path = file_path;
+    }
+
+    int readLines() throws IOException {
+        FileReader file_to_read = new FileReader(path);
+        BufferedReader bf = new BufferedReader(file_to_read);
+        String aLine;
+        int numberOfLines = 0;
+        while ((aLine = bf.readLine()) != null) {
+            numberOfLines++;
+        }
+        bf.close();
+        return numberOfLines;
+    }
+
+    public ArrayList OpenFile() throws IOException {
+
+        FileReader fr = new FileReader(path);
+        BufferedReader textReader = new BufferedReader(fr);
+        int numberOfLines = readLines();
+        ArrayList textData = new ArrayList();
+        String[] clause;
+        ArrayList intData = new ArrayList<Integer>();
+
+        for (int i = 0; i < numberOfLines; i++) {
+            String currentLine = textReader.readLine();
+
+            if (currentLine != null) {
+
+                if (!(currentLine.charAt(0) == ('c')) && !(currentLine.charAt(0) == ('p'))) {
+                    clause = currentLine.replace(" 0", "").split("\\s+");
+                    List<String> stringList = new ArrayList<>(Arrays.asList(clause));
+                    for (String s : stringList) intData.add(Integer.valueOf(s));
+                    textData.add(stringList);
+                }
+            }
+        }
+        textReader.close();
+       return textData;
+
+    }
+}
+
+//    //parses string array to int array
+//    public static int[] parseIntArray(String[] arr) {
+//        return Stream.of(arr).mapToInt(Integer::parseInt).toArray();
+//    }
+
+
